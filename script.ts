b5fb749adf336b9a7b5308dce8e9313d97f3be1d
AstecNIcal <166935816+AstecNIcal@users.noreply.github.com>
2024-11-11 00:25:26 +0500

    Add files via upload

--- script.ts
+++ script.ts
@@ -1,13 +1,55 @@
-const Myskills= document.getElementById('Myskills') as HTMLButtonElement
 
-const skills = document.getElementById('skills') as HTMLElement
+let form =document.getElementById('resume-form') as HTMLFormElement;
+    let resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+  
+    /* //handle form submission */
+    form.addEventListener('submit', (event: Event) => {
+      event.preventDefault(); //prevent page reload
+  
+      /* //Collect input values */
+      const name = (document.getElementById('name') as HTMLInputElement).value
+      const email = (document.getElementById('email') as HTMLInputElement).value
+      const phone = (document.getElementById('phone') as HTMLInputElement).value
+      const education = (document.getElementById('education') as HTMLInputElement).value
+      const experience = (document.getElementById('experience') as HTMLInputElement).value
+      const skills = (document.getElementById('skills') as HTMLInputElement).value
+  
+      /* // Generate the resume content dynamically */
+      const resumeHtml = `
+        <h2> <b> Editable Resume </b> </h2>
+        <h3>Personal Information</h3>
+        <p><b>Name:</b> <span contenteditable> ${name} </span></p>
+        <p><b>Email:</b><span contenteditable> ${email}</span></p>
+        <p><b>Phone:</b>  <span contenteditable> ${phone}</span></p>
+  
+        <h3>Education</h3>
+        <pcontenteditable="true">${education}</p>
+  
+        <h3>Experience</h3>
+        <p contenteditable="true">${experience}</p>
+  
+        <h3>Skills</h3>
+        <p contenteditable="true">${skills}</p>
+      `;
+  
+      /* //Display the generated resume */
+      if(resumeDisplayElement){
+        resumeDisplayElement.innerHTML = resumeHtml;
+      }else {
+        console.error('The resume display element is missing.');
+      };
+    });
 
-Myskills.addEventListener('click',()=>{
-if(skills.style.display === 'none'){
-skills.style.display = 'block'
-}else {
-skills.style.display = 'none'
-}
 
-});
+
+
+
+
+
+
+
+
+
+
+
 
